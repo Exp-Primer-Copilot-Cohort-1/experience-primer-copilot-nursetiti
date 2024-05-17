db2bb7deeea6f3c1bc2562141b1f1e47765eba0b
Alade Nosirat <nosiratalade1@gmail.com>
2024-05-17 15:17:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,8 @@
-//create
+//create web server 
+var http = require('http');
 
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
